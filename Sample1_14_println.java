2fa675684741c900df08663ee9e0509b04359445
yujimiya <yuji.miyamoto32@gmail.com>
2024-05-08 21:22:54 +0900

    クラスとメソッド④

---
+++ Sample1_14_println.java
@@ -0,0 +1,29 @@
+class Sample1_14_println {
+	
+	public static void main(String[] args) {
+		
+		//printlnはオーバーロードされて定義されている
+		
+		System.out.println("▼print1（int型）");
+		int print1 = 1 ;
+		System.out.println( print1 );
+		
+		System.out.println("▼print2（double型）");
+		double print2 = 2.0 ;
+		System.out.println( print2 );
+		
+		System.out.println("▼print3（char型）");
+		char print3 = '3' ;
+		System.out.println( print3 );
+		
+		System.out.println("▼print4（String型）");
+		String print4 = "4" ;
+		System.out.println( print4 );
+		
+		System.out.println("▼print5（boolean型）");
+		boolean print5 = true ;
+		System.out.println( print5 );
+		
+	}
+	
+}
